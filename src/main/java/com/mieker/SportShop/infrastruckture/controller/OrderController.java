f521eb0ab30d95edd9d2f6f3540aeb9cb7ca1926
Mieker <mieker@wp.pl>
2023-06-12 19:56:39 +0200

    controller fulfilled with authorization logic;

--- src/main/java/com/mieker/SportShop/infrastruckture/controller/OrderController.java
+++ src/main/java/com/mieker/SportShop/infrastruckture/controller/OrderController.java
@@ -1,27 +1,44 @@
 package com.mieker.SportShop.infrastruckture.controller;
 
+import com.mieker.SportShop.application.dto.order.OrderDto;
+import com.mieker.SportShop.application.dto.order.RequestOrderDto;
+import com.mieker.SportShop.application.dto.user.UserDto;
+import com.mieker.SportShop.application.service.AuthorizationService;
 import com.mieker.SportShop.application.service.OrderService;
+import com.mieker.SportShop.domain.model.user.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
 public class OrderController {
 
+    private final AuthorizationService authorizationService;
     private final OrderService orderService;
 
-    public ResponseEntity<?> createOrder() {
-        return null;
+    @PostMapping
+    public ResponseEntity<OrderDto> createOrder(
+            @RequestHeader("Authorization") String authorizationHeader, @RequestBody RequestOrderDto order) {
+        UserDto userDto = authorizationService.authorizeUser(authorizationHeader, List.of(Role.ADMIN, Role.CUSTOMER));
+        return ResponseEntity.ok(orderService.createOrder(order, userDto.getId()));
     }
 
-    public ResponseEntity<?> getOrders() {
-        return null;
+    @GetMapping
+    public ResponseEntity<List<OrderDto>> getOrders(
+            @RequestHeader("Authorization") String authorizationHeader, @RequestParam String customerId) {
+        UserDto userDto = authorizationService.authorizeUser(authorizationHeader, List.of(Role.ADMIN, Role.CUSTOMER));
+        return ResponseEntity.ok(orderService.getOrders(userDto, customerId));
     }
 
-    public ResponseEntity<?> deleteOrder() {
-        return null;
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<String> deleteOrder(
+            @RequestHeader("Authorization") String authorizationHeader, @PathVariable String orderId) {
+        authorizationService.authorizeUser(authorizationHeader, List.of(Role.ADMIN));
+        orderService.deleteOrder(orderId);
+        return ResponseEntity.ok().body("Order " + orderId + " deleted.");
     }
 }
